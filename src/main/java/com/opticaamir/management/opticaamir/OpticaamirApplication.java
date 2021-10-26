7f59527b33c63977275967fbc5f8c210899080b6
20174374 <luiscarvajal2208@gmail.com>
2021-10-26 18:30:31 -0300

    creation of entity, model and controller structure

---
+++ src/main/java/com/opticaamir/management/opticaamir/OpticaamirApplication.java
@@ -0,0 +1,13 @@
+package com.opticaamir.management.opticaamir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpticaamirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpticaamirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opticaamir/management/opticaamir/OpticaamirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opticaamir.management.opticaamir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpticaamirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
